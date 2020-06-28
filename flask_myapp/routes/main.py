f1dcf7dd2e71811786191e53c06229e0d34d8cec
Maitree Hirunteeyakun <h.mattjunk@gmail.com>
2020-06-28 21:03:35 +0700

    del line logger.info

--- flask_myapp/routes/main.py
+++ flask_myapp/routes/main.py
@@ -83,7 +83,6 @@ def linewebhook():
     signature = request.headers['X-Line-Signature']
     # get request body as text
     body = request.get_data(as_text=True)
-    current_app.logger.info("Request body: " + body)
     # handle webhook body
     try:
         handler.handle(body, signature)
